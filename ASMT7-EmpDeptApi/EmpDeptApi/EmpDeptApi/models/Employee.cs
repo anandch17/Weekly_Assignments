e5460d440f7086c2cd9dfcb16232748a3ada25a3
anandch17 <160841358+anandch17@users.noreply.github.com>
2026-02-16 14:08:48 +0530

    Assignment 7 Scaffolding web api

---
+++ ASMT7-EmpDeptApi/EmpDeptApi/EmpDeptApi/models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmpDeptApi.models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public int Salary { get; set; }
+    }
+}
